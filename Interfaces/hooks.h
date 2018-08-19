f791709fe3b3d2694bb4c5c9c3ffd4a0e3ba0d7c
V1RuS #6751 <42515727+V1RuSGR@users.noreply.github.com>
2018-08-19 22:03:21 +0800

    Hooks.h
    Added working sigs.

--- Interfaces/hooks.h
+++ Interfaces/hooks.h
@@ -6,8 +6,8 @@ extern void InitializeHooks();      //  Handles all hooks
 extern void UpdateResolver();       //
 extern void PrintInfo();            //  Prints cheat info
 
-#define CLIENTMODE_SIG  "\xE8\x00\x00\x00\x00\x48\xC7\xC3\x00\x00\x00\x00\x4C\x8D\x3D\x00\x00\x00\x00\x0F\x1F\x44\x00"
-#define CLIENTMODE_MASK "x????xxx????xxx????xxxx"
+#define CLIENTMODE_SIG  "\x48\x8B\xB7\x00\x00\x00\x00\x48\x8D\x3D\x00\x00\x00\x00\x5D\xE9"
+#define CLIENTMODE_MASK "xxx????xxx????xx"
 
 #define GLOBALS_SIG     "\x48\x8D\x05\x00\x00\x00\x00\x48\x8B\x00\xF3\x0F\x10\x00\x00\xF3\x0F\x11\x83\x00\x00\x00\x00"
 #define GLOBALS_MASK    "xxx????xxxxxx??xxxx????"
